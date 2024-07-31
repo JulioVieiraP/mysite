ce0529ff3fe168c12ae09e578a17b72955375a1c
JulioVieiraP <134452045+JulioVieiraP@users.noreply.github.com>
2024-07-31 11:44:06 -0300

    feat: criando os templates e configurando o projeto

--- mysite/blog/views/Home_view.py
+++ mysite/blog/views/Home_view.py
@@ -1,6 +1,11 @@
-from django.http import HttpResponse
 from django.views import generic
 
-class HomeView(generic.View):
-    def get(self, request, *args, **kwargs):
-        return HttpResponse("Hello World!")
+from blog.models import Post
+
+class HomeView(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
--- mysite/blog/views/__init__.py
+++ mysite/blog/views/__init__.py
@@ -1,1 +1,2 @@
 from .Home_view import HomeView
+from .Home_view import PostDetail
