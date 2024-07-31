aef81e944eef6d2e16c9fd5b467fbac398c62149
JulioVieiraP <134452045+JulioVieiraP@users.noreply.github.com>
2024-07-31 11:54:26 -0300

    fix: corrigindo conflitos de digitação

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,6 +2,6 @@ from blog import views
 from django.urls import path
 
 urlpatterns = [
-    path('' , views.HomeView.as_view(), name='home'),
+    path('' , views.PostView.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
--- mysite/blog/views/__init__.py
+++ mysite/blog/views/__init__.py
@@ -1,2 +1,2 @@
-from .Home_view import HomeView
-from .Home_view import PostDetail
+from .post_view import PostView
+from .post_view import PostDetail
